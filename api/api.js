a897cd1b1d1d2c1358bfa6a213d1b1bfb155a2aa
Dan Pastusek <dan@hexial.com>
2018-01-10 22:42:06 -0800

    Add stock market support via AlphaVantage API

--- api/api.js
+++ api/api.js
@@ -6,7 +6,8 @@ const sub = redis.createClient();
 const wss = new WebSocket.Server({port: 8080});
 
 // TODO keying subs based on symbol will create some bugs because there are duplicate coins with the same symbol
-const subscriptions = {};
+const cryptoSubscriptions = {};
+const stockSubscriptions = {};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function (message) {
@@ -17,56 +18,100 @@ wss.on('connection', function connection(ws) {
       case 'crypto-sub':
         handleCryptoSub(payload);
         break;
+      case 'stock-sub':
+        handleStockSub(payload);
+        break;
     }
 
   });
 
   function handleCryptoSub({symbol, requireLatest}) {
-    // TODO for stocks - Check if symbol exists in redis zset 'followed'. If so, proceed. If not, make coinmarketcap call here.
-    if (subscriptions[symbol]) {
-      subscriptions[symbol].push(ws)
+    if (cryptoSubscriptions[symbol]) {
+      cryptoSubscriptions[symbol].push(ws)
     } else {
-      subscriptions[symbol] = [ws]
-      // TODO for stocks - SUB to redis stock channel
+      cryptoSubscriptions[symbol] = [ws]
     }
 
-    // TODO for stocks - update redis zset 'followed' with ticker and current timestamp
-
-    redisClient.get(`latest:${symbol}`, function (err, response) {
+    redisClient.get(`latest:crypto:${symbol}`, function (err, response) {
       if (err) throw err;
 
       if (response === null) {
         ws.send(JSON.stringify(['crypto-unsub', symbol]));
-      } else if (requireLatest) {
+        return
+      }
+
+      if (requireLatest) {
         ws.send(JSON.stringify(['crypto-update', response]));
       }
     });
   }
 
-  redisClient.get('top', function (err, response) {
+  function handleStockSub({symbol, requireLatest}) {
+    // TODO - Check if symbol exists in redis zset 'followed'. If so, proceed. If not, make coinmarketcap call here.
+    if (stockSubscriptions[symbol]) {
+      stockSubscriptions[symbol].push(ws)
+    } else {
+      stockSubscriptions[symbol] = [ws]
+    }
+
+    // TODO - update redis zset 'followed' with ticker and current timestamp so the worker can track only a handful of most recently subscribed stocks
+
+    redisClient.get(`latest:stock:${symbol}`, function (err, response) {
+      if (err) throw err;
+
+      if (response === null) {
+        ws.send(JSON.stringify(['stock-unsub', symbol]));
+        return;
+      }
+
+      if (requireLatest) {
+        ws.send(JSON.stringify(['stock-update', response]));
+      }
+    });
+  }
+
+  redisClient.get('top:crypto', function (err, response) {
     if (err) throw err;
-    ws.send(JSON.stringify(['top', response]));
+    ws.send(JSON.stringify(['crypto-top', response]));
   });
 
-  // TODO for timeseries data
-  // const args = [CACHE_COIN_PREFIX + 'BTC', '+inf', '-inf', 'LIMIT', 1];
+  redisClient.hgetall('top:stock', function (err, response) {
+    if (err) throw err;
+    ws.send(JSON.stringify(['stock-top', response]));
+  });
+
+  // TODO for timeseries data / charts
+  // const args = [ `historical:BTC`, '+inf', '-inf', 'LIMIT', 1];
   // client.zrevrangebyscore(args, function (err, response) {
   //   if (err) throw err;
   // });
 
   function close() {
-    for (const symbol in subscriptions) {
-      const index = subscriptions[symbol].indexOf(ws);
+    for (const symbol in cryptoSubscriptions) {
+      const index = cryptoSubscriptions[symbol].indexOf(ws);
 
       if (index === -1) {
         return;
       }
 
-      subscriptions[symbol].splice(index, 1);
+      cryptoSubscriptions[symbol].splice(index, 1);
 
-      if (subscriptions[symbol].length === 0) {
-        delete subscriptions[symbol];
-        //TODO for stocks - unsubscribe this API server from this stock's channel on redis
+      if (cryptoSubscriptions[symbol].length === 0) {
+        delete cryptoSubscriptions[symbol];
+      }
+    }
+
+    for (const symbol in stockSubscriptions) {
+      const index = stockSubscriptions[symbol].indexOf(ws);
+
+      if (index === -1) {
+        return;
+      }
+
+      stockSubscriptions[symbol].splice(index, 1);
+
+      if (stockSubscriptions[symbol].length === 0) {
+        delete stockSubscriptions[symbol];
       }
     }
   }
@@ -76,11 +121,14 @@ wss.on('connection', function connection(ws) {
   ws.on('error', close);
 });
 
-sub.subscribe("crypto-updates");
+sub.subscribe(['crypto-updates', 'stock-updates']);
 sub.on("message", function (channel, message) {
   switch (channel) {
     case 'crypto-updates':
       broadcastCryptoUpdates(message);
+      break;
+    case 'stock-updates':
+      broadcastStockUpdates(message);
       break;
   }
 });
@@ -90,13 +138,30 @@ function broadcastCryptoUpdates(data) {
   let broadcastCount = 0;
   coins.forEach(coin => {
     const msg = JSON.stringify(['crypto-update', JSON.stringify(coin)]);
-    const clientList = subscriptions[coin.symbol];
+    const clientList = cryptoSubscriptions[coin.symbol];
     clientList && clientList.forEach(ws => {
       broadcastCount++;
       ws.send(msg);
     });
   });
   console.log(`Received crypto update with ${coins.length} coins. Broadcasted ${broadcastCount} messages.`)
+}
+
+function broadcastStockUpdates(data) {
+  console.log('stock update', data);
+  const stock = JSON.parse(data);
+  let broadcastCount = 0;
+
+  stock.price = 11.11;
+  data = JSON.stringify(stock);
+
+  const msg = JSON.stringify(['stock-update', data]);
+  const clientList = stockSubscriptions[stock.symbol];
+  clientList && clientList.forEach(ws => {
+    broadcastCount++;
+    ws.send(msg);
+  });
+  console.log(`Received stock update for ${stock.symbol}. Broadcasted ${broadcastCount} messages.`)
 }
 
 // TODO for debugging -- remove
--- api/worker.js
+++ api/worker.js
@@ -2,33 +2,55 @@ const redis = require('redis');
 const request = require('request');
 const async = require('async');
 const now = require("performance-now");
+const _ = require('lodash');
 
 const redisClient = redis.createClient();
-const API_URL = 'https://api.coinmarketcap.com/v1/ticker/?limit=10000';
+const trackedStocks = [
+  'AAPL',
+  'GOOG',
+  'MSFT',
+  'AMZN',
+  'BRK.A',
+  'BABA',
+  'TCEHY',
+  'FB',
+  'XOM',
+  'JNJ',
+];
+const CRYPTO_API_URL = 'https://api.coinmarketcap.com/v1/ticker/?limit=10000';
+const getStockAPIURL = function (symbol) {
+  return `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${process.env.STOCK_API_KEY}`;
+};
 
 // Respect the CoinMarketCap API limit of 10 requests per minute
-const refreshIntervalMs = 1000 * 60 / 10;
+const cryptoRefreshIntervalMs = 1000 * 60 / 10;
+// AlphaVantage asks for no more than 1 per second, but we only track 10 stocks and they update every minute.
+// Their responses take ~5-20 sec anyway
+const stockRefreshIntervalMs = 1000 * 60;
 
-let req;
+let cryptoReq;
+const stockReqs = {};
 
-function fetchMarketData() {
+function fetchCryptoData() {
   // kill the existing request if its still pending
-  req && req.abort();
+  cryptoReq && cryptoReq.abort();
 
-  req = request(API_URL, function (error, response, body) {
-    if (error) console.log('API fetch error: ', error);
+  cryptoReq = request(CRYPTO_API_URL, function (error, response, body) {
+    if (error) console.log('Crypto API fetch error: ', error);
 
-    redisClient.get('latest', function (err, lastData) {
-      if (lastData === body) {
+    redisClient.get('latest:crypto', function (err, lastBody) {
+      // If response is identical to the last stored response in redis then don't proceed.
+      // Useful since CoinMarketCap only updates every ~5min.
+      if (lastBody === body) {
         return;
       }
 
       const coins = JSON.parse(body);
-      const lastCoins = lastData ? JSON.parse(lastData) : [];
-      const top = coins.slice(0, 20);
+      const lastCoins = lastBody ? JSON.parse(lastBody) : [];
+      const top = coins.slice(0, 10);
 
-      redisClient.set('top', JSON.stringify(top));
-      redisClient.set('latest', body);
+      redisClient.set('top:crypto', JSON.stringify(top));
+      redisClient.set('latest:crypto', body);
 
       //check each coin, see if the attributes we care about are different, and dedupe the broadcast
       const lastCoinSymbolMap = lastCoins.map(({id}) => id);
@@ -42,7 +64,7 @@ function fetchMarketData() {
         });
       });
 
-      console.log('Broadcasting updates. Updated coin count: ', updatedCoins.length);
+      console.log(`CRYPTO: Broadcasting updates. Updated coin count: ${updatedCoins.length} / ${coins.length}`);
       redisClient.publish('crypto-updates', JSON.stringify(updatedCoins));
 
       const start = now();
@@ -56,7 +78,7 @@ function fetchMarketData() {
 
         // store historical data for USD, BTC, and Volume
         redisClient.zadd([
-          `historical:${coin.symbol}`,
+          `historical:crypto:${coin.symbol}`,
           coin.last_updated,
           JSON.stringify({
             usd: coin['price_usd'],
@@ -69,22 +91,116 @@ function fetchMarketData() {
           callback();
         });
       }, function () {
-        console.log(`updated historical data in ${now() - start}ms`);
+        console.log(`CRYPTO: Updated historical data in ${now() - start}ms`);
       });
 
       async.eachLimit(coins, 10, function (coin, callback) {
-        redisClient.set(`latest:${coin.symbol}`, JSON.stringify(coin), function (err) {
+        redisClient.set(`latest:crypto:${coin.symbol}`, JSON.stringify(coin), function (err) {
           if (err) throw err;
           callback();
         });
       }, function () {
-        console.log(`added all individual latest coins in ${now() - start}ms`);
+        console.log(`CRYPTO: Added all individual latest coins in ${now() - start}ms`);
       });
 
       const time = new Date().getTime() / 1000;
-      console.log(`New data. BTC Age: ${time - coins[0].last_updated}`);
+      console.log(`CRYPTO: New data. BTC Age: ${time - coins[0].last_updated}`);
     });
   });
 }
 
-setInterval(fetchMarketData, refreshIntervalMs);
+function fetchStockData() {
+  trackedStocks.forEach(symbol => {
+    // kill the existing request if its still pending
+    stockReqs[symbol] && stockReqs[symbol].abort();
+
+    const start = now();
+
+    stockReqs[symbol] = request(getStockAPIURL(symbol), function (error, response, body) {
+      if (error) console.log('Stock API fetch error: ', error);
+
+      redisClient.get(`latest:stock:http-res:${symbol}`, function (err, lastBody) {
+        // If response is identical to the last stored response in redis then don't proceed.
+        // Useful for when the market is closed.
+        if (lastBody === body) {
+          return;
+        }
+
+        redisClient.set(`latest:stock:http-res:${symbol}`, body);
+
+        body = JSON.parse(body);
+
+        console.log(`STOCK: Fetched updated data for ${symbol} in ${now() - start}ms`);
+
+        // Check validity of response... sometimes we get bad data (like an HTML body) as a response
+        if (!body['Time Series (1min)']) {
+          return;
+        }
+
+        const timestamps = Object.keys(body['Time Series (1min)']);
+        const data = body['Time Series (1min)'];
+
+        const latestData = {
+          symbol,
+          price: data[timestamps[0]]['4. close'],
+          volume: data[timestamps[0]]['5. volume'],
+        };
+
+        // look at historical data to calculate % change
+        async.each([{
+          time: new Date() - 3600 * 1000,
+          key: 'percent_change_1h'
+        }, {
+          time: new Date() - 24 * 3600 * 1000,
+          key: 'percent_change_24h'
+        }, {
+          time: new Date() - 7 * 24 * 3600 * 1000,
+          key: 'percent_change_7d'
+        }], function ({time, key}, callback) {
+          const unixTimestamp = Math.round(new Date(time).getTime() / 1000);
+          redisClient.zrevrangebyscore([`historical:stocks:${symbol}`, unixTimestamp, '-inf', 'LIMIT', 0, 1], function (err, historicalData) {
+            if (err) throw err;
+
+            if (historicalData.length === 0) {
+              latestData[key] = 0;
+            } else {
+              const latestPrice = parseFloat(latestData.price);
+              const historicalPrice = parseFloat(JSON.parse(historicalData[0]).price);
+              latestData[key] = (latestPrice - historicalPrice) / historicalPrice;
+            }
+            callback();
+          });
+        }, function () {
+          redisClient.hset("top:stock", symbol, JSON.stringify(latestData));
+          redisClient.set(`latest:stock:${symbol}`, JSON.stringify(latestData));
+          redisClient.publish('stock-updates', JSON.stringify({
+            symbol,
+            data: latestData
+          }));
+        });
+
+        // Add and update historical data
+        const minuteData = timestamps.map(time => {
+          const unixTimestamp = Math.round(new Date(time).getTime() / 1000);
+          return {
+            price: data[time]['4. close'],
+            volume: data[time]['5. volume'],
+            timestamp: unixTimestamp
+          }
+        });
+        const unixTimestamps = minuteData.map(({timestamp}) => timestamp);
+        const zippedMinuteData = _.flatten(_.zip(unixTimestamps, minuteData.map(data => JSON.stringify(data))));
+        const zaddData = [`historical:stocks:${symbol}`].concat(zippedMinuteData);
+        redisClient.zadd(zaddData, function (err) {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+}
+
+fetchCryptoData();
+setInterval(fetchCryptoData, cryptoRefreshIntervalMs);
+
+fetchStockData();
+setInterval(fetchStockData, stockRefreshIntervalMs);
