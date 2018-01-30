ac6328fed42bd3f71e2a0b07011d07f65e24371e
Dan Pastusek <dan@hexial.com>
2018-01-30 02:04:07 -0800

    remove BTC links

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-REACT_APP_SOCKET_URL=wss://live.altfolio.live && yarn build
+
+# deploy API
+ssh 35.202.251.208 "cd altfolio-live && git pull && yarn && sudo pm2 kill && sudo -E pm2 start ecosystem.config.js"
+
+# build and deploy client
+REACT_APP_SOCKET_URL=wss://live.altfolio.live yarn build
 aws s3 sync build/ s3://altfolio.live
