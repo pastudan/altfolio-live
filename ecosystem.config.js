679befa5b0a1795141440206cdc0671412c02788
Dan Pastusek <dan@hexial.com>
2018-01-30 02:09:38 -0800

    log format per app

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,16 +5,22 @@ module.exports = {
       script: 'api/api.js',
       max_restarts: 10,
       min_uptime: 5000,
+      log_date_format: 'YYYY-MM-DD HH:mm Z'
+
     }, {
       name: 'Stock Worker',
       script: 'api/stock-worker.js',
       max_restarts: 10,
       min_uptime: 5000,
+      log_date_format: 'YYYY-MM-DD HH:mm Z'
+
     }, {
       name: 'Crypto Worker',
       script: 'api/crypto-worker.js',
       max_restarts: 10,
       min_uptime: 5000,
+      log_date_format: 'YYYY-MM-DD HH:mm Z'
+
     }
   ],
 }
