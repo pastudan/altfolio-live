5fb14dae326b74359f6a3b2ee59a70b234a5eec2
Dan Pastusek <dan@hexial.com>
2018-02-02 12:15:03 -0800

    rebrand XRB to NANO in footer donation

--- src/images/coinLogos.js
+++ src/images/coinLogos.js
@@ -14,7 +14,9 @@ import logoBCC from 'cryptocurrency-icons/svg/color/bcc.svg'
 import logoBCH from 'cryptocurrency-icons/svg/color/bch.svg'
 import logoBCN from 'cryptocurrency-icons/svg/color/bcn.svg'
 import logoBDL from 'cryptocurrency-icons/svg/color/bdl.svg'
+import logoBELA from 'cryptocurrency-icons/svg/color/bela.svg'
 import logoBLCN from 'cryptocurrency-icons/svg/color/blcn.svg'
+import logoBLK from 'cryptocurrency-icons/svg/color/blk.svg'
 import logoBLOCK from 'cryptocurrency-icons/svg/color/block.svg'
 import logoBNB from 'cryptocurrency-icons/svg/color/bnb.svg'
 import logoBNT from 'cryptocurrency-icons/svg/color/bnt.svg'
@@ -27,11 +29,20 @@ import logoBTG from 'cryptocurrency-icons/svg/color/btg.svg'
 import logoBTM from 'cryptocurrency-icons/svg/color/btm.svg'
 import logoBTS from 'cryptocurrency-icons/svg/color/bts.svg'
 import logoBTX from 'cryptocurrency-icons/svg/color/btx.svg'
+import logoBURST from 'cryptocurrency-icons/svg/color/burst.svg'
+import logoCDN from 'cryptocurrency-icons/svg/color/cdn.svg'
+import logoCLAM from 'cryptocurrency-icons/svg/color/clam.svg'
+import logoCLOAK from 'cryptocurrency-icons/svg/color/cloak.svg'
+import logoCND from 'cryptocurrency-icons/svg/color/cnd.svg'
 import logoCNX from 'cryptocurrency-icons/svg/color/cnx.svg'
 import logoCNY from 'cryptocurrency-icons/svg/color/cny.svg'
+import logoCRED from 'cryptocurrency-icons/svg/color/cred.svg'
+import logoCRPT from 'cryptocurrency-icons/svg/color/crpt.svg'
 import logoCVC from 'cryptocurrency-icons/svg/color/cvc.svg'
 import logoDASH from 'cryptocurrency-icons/svg/color/dash.svg'
+import logoDAT from 'cryptocurrency-icons/svg/color/dat.svg'
 import logoDATA from 'cryptocurrency-icons/svg/color/data.svg'
+import logoDBC from 'cryptocurrency-icons/svg/color/dbc.svg'
 import logoDCN from 'cryptocurrency-icons/svg/color/dcn.svg'
 import logoDCR from 'cryptocurrency-icons/svg/color/dcr.svg'
 import logoDENT from 'cryptocurrency-icons/svg/color/dent.svg'
@@ -42,6 +53,7 @@ import logoDRGN from 'cryptocurrency-icons/svg/color/drgn.svg'
 import logoEDG from 'cryptocurrency-icons/svg/color/edg.svg'
 import logoEDOGE from 'cryptocurrency-icons/svg/color/edoge.svg'
 import logoELF from 'cryptocurrency-icons/svg/color/elf.svg'
+import logoELLA from 'cryptocurrency-icons/svg/color/ella.svg'
 import logoEMC from 'cryptocurrency-icons/svg/color/emc.svg'
 import logoEMC2 from 'cryptocurrency-icons/svg/color/emc2.svg'
 import logoENG from 'cryptocurrency-icons/svg/color/eng.svg'
@@ -52,28 +64,41 @@ import logoETHOS from 'cryptocurrency-icons/svg/color/ethos.svg'
 import logoETN from 'cryptocurrency-icons/svg/color/etn.svg'
 import logoETP from 'cryptocurrency-icons/svg/color/etp.svg'
 import logoEUR from 'cryptocurrency-icons/svg/color/eur.svg'
+import logoEVX from 'cryptocurrency-icons/svg/color/evx.svg'
 import logoEXMO from 'cryptocurrency-icons/svg/color/exmo.svg'
+import logoEXP from 'cryptocurrency-icons/svg/color/exp.svg'
 import logoFAIR from 'cryptocurrency-icons/svg/color/fair.svg'
 import logoFCT from 'cryptocurrency-icons/svg/color/fct.svg'
 import logoFIL from 'cryptocurrency-icons/svg/color/fil.svg'
+import logoFLDC from 'cryptocurrency-icons/svg/color/fldc.svg'
+import logoFLO from 'cryptocurrency-icons/svg/color/flo.svg'
 import logoFUN from 'cryptocurrency-icons/svg/color/fun.svg'
 import logoGAME from 'cryptocurrency-icons/svg/color/game.svg'
 import logoGAS from 'cryptocurrency-icons/svg/color/gas.svg'
 import logoGBP from 'cryptocurrency-icons/svg/color/gbp.svg'
+import logoGBX from 'cryptocurrency-icons/svg/color/gbx.svg'
 import logoGBYTE from 'cryptocurrency-icons/svg/color/gbyte.svg'
 import logoGNO from 'cryptocurrency-icons/svg/color/gno.svg'
 import logoGNT from 'cryptocurrency-icons/svg/color/gnt.svg'
+import logoGRC from 'cryptocurrency-icons/svg/color/grc.svg'
 import logoGRS from 'cryptocurrency-icons/svg/color/grs.svg'
 import logoGUP from 'cryptocurrency-icons/svg/color/gup.svg'
 import logoGXS from 'cryptocurrency-icons/svg/color/gxs.svg'
 import logoHSR from 'cryptocurrency-icons/svg/color/hsr.svg'
+import logoHUC from 'cryptocurrency-icons/svg/color/huc.svg'
+import logoHUSH from 'cryptocurrency-icons/svg/color/hush.svg'
 import logoICN from 'cryptocurrency-icons/svg/color/icn.svg'
 import logoICX from 'cryptocurrency-icons/svg/color/icx.svg'
+import logoINS from 'cryptocurrency-icons/svg/color/ins.svg'
+import logoIOST from 'cryptocurrency-icons/svg/color/iost.svg'
 import logoJPY from 'cryptocurrency-icons/svg/color/jpy.svg'
 import logoKCS from 'cryptocurrency-icons/svg/color/kcs.svg'
+import logoKIN from 'cryptocurrency-icons/svg/color/kin.svg'
 import logoKMD from 'cryptocurrency-icons/svg/color/kmd.svg'
 import logoKNC from 'cryptocurrency-icons/svg/color/knc.svg'
 import logoKRB from 'cryptocurrency-icons/svg/color/krb.svg'
+import logoLBC from 'cryptocurrency-icons/svg/color/lbc.svg'
+import logoLINK from 'cryptocurrency-icons/svg/color/link.svg'
 import logoLKK from 'cryptocurrency-icons/svg/color/lkk.svg'
 import logoLRC from 'cryptocurrency-icons/svg/color/lrc.svg'
 import logoLSK from 'cryptocurrency-icons/svg/color/lsk.svg'
@@ -82,23 +107,40 @@ import logoMAID from 'cryptocurrency-icons/svg/color/maid.svg'
 import logoMANA from 'cryptocurrency-icons/svg/color/mana.svg'
 import logoMCAP from 'cryptocurrency-icons/svg/color/mcap.svg'
 import logoMCO from 'cryptocurrency-icons/svg/color/mco.svg'
+import logoMED from 'cryptocurrency-icons/svg/color/med.svg'
 import logoMIOTA from 'cryptocurrency-icons/svg/color/miota.svg'
+import logoMKR from 'cryptocurrency-icons/svg/color/mkr.svg'
 import logoMLN from 'cryptocurrency-icons/svg/color/mln.svg'
 import logoMNX from 'cryptocurrency-icons/svg/color/mnx.svg'
 import logoMONA from 'cryptocurrency-icons/svg/color/mona.svg'
 import logoMTL from 'cryptocurrency-icons/svg/color/mtl.svg'
 import logoMUSIC from 'cryptocurrency-icons/svg/color/music.svg'
+import logoNANO from 'cryptocurrency-icons/svg/color/nano.svg'
+import logoNAS from 'cryptocurrency-icons/svg/color/nas.svg'
 import logoNAV from 'cryptocurrency-icons/svg/color/nav.svg'
+import logoNDZ from 'cryptocurrency-icons/svg/color/ndz.svg'
 import logoNEBL from 'cryptocurrency-icons/svg/color/nebl.svg'
 import logoNEO from 'cryptocurrency-icons/svg/color/neo.svg'
+import logoNEOS from 'cryptocurrency-icons/svg/color/neos.svg'
+import logoNGC from 'cryptocurrency-icons/svg/color/ngc.svg'
+import logoNLC2 from 'cryptocurrency-icons/svg/color/nlc2.svg'
 import logoNLG from 'cryptocurrency-icons/svg/color/nlg.svg'
 import logoNMC from 'cryptocurrency-icons/svg/color/nmc.svg'
 import logoNXS from 'cryptocurrency-icons/svg/color/nxs.svg'
 import logoNXT from 'cryptocurrency-icons/svg/color/nxt.svg'
 import logoOMG from 'cryptocurrency-icons/svg/color/omg.svg'
+import logoOMNI from 'cryptocurrency-icons/svg/color/omni.svg'
+import logoOST from 'cryptocurrency-icons/svg/color/ost.svg'
+import logoOX from 'cryptocurrency-icons/svg/color/ox.svg'
+import logoPAC from 'cryptocurrency-icons/svg/color/pac.svg'
 import logoPART from 'cryptocurrency-icons/svg/color/part.svg'
+import logoPASL from 'cryptocurrency-icons/svg/color/pasl.svg'
 import logoPAY from 'cryptocurrency-icons/svg/color/pay.svg'
+import logoPINK from 'cryptocurrency-icons/svg/color/pink.svg'
+import logoPIRL from 'cryptocurrency-icons/svg/color/pirl.svg'
 import logoPIVX from 'cryptocurrency-icons/svg/color/pivx.svg'
+import logoPLR from 'cryptocurrency-icons/svg/color/plr.svg'
+import logoPOE from 'cryptocurrency-icons/svg/color/poe.svg'
 import logoPOT from 'cryptocurrency-icons/svg/color/pot.svg'
 import logoPOWR from 'cryptocurrency-icons/svg/color/powr.svg'
 import logoPPC from 'cryptocurrency-icons/svg/color/ppc.svg'
@@ -106,207 +148,302 @@ import logoPPT from 'cryptocurrency-icons/svg/color/ppt.svg'
 import logoPURA from 'cryptocurrency-icons/svg/color/pura.svg'
 import logoQASH from 'cryptocurrency-icons/svg/color/qash.svg'
 import logoQIWI from 'cryptocurrency-icons/svg/color/qiwi.svg'
+import logoQLC from 'cryptocurrency-icons/svg/color/qlc.svg'
+import logoQSP from 'cryptocurrency-icons/svg/color/qsp.svg'
 import logoQTUM from 'cryptocurrency-icons/svg/color/qtum.svg'
+import logoRADS from 'cryptocurrency-icons/svg/color/rads.svg'
 import logoRCN from 'cryptocurrency-icons/svg/color/rcn.svg'
 import logoRDD from 'cryptocurrency-icons/svg/color/rdd.svg'
 import logoRDN from 'cryptocurrency-icons/svg/color/rdn.svg'
 import logoREP from 'cryptocurrency-icons/svg/color/rep.svg'
 import logoREQ from 'cryptocurrency-icons/svg/color/req.svg'
 import logoRHOC from 'cryptocurrency-icons/svg/color/rhoc.svg'
+import logoRIC from 'cryptocurrency-icons/svg/color/ric.svg'
+import logoRISE from 'cryptocurrency-icons/svg/color/rise.svg'
 import logoRLC from 'cryptocurrency-icons/svg/color/rlc.svg'
+import logoRPX from 'cryptocurrency-icons/svg/color/rpx.svg'
 import logoRUB from 'cryptocurrency-icons/svg/color/rub.svg'
 import logoSALT from 'cryptocurrency-icons/svg/color/salt.svg'
 import logoSAN from 'cryptocurrency-icons/svg/color/san.svg'
+import logoSBD from 'cryptocurrency-icons/svg/color/sbd.svg'
 import logoSBERBANK from 'cryptocurrency-icons/svg/color/sberbank.svg'
 import logoSC from 'cryptocurrency-icons/svg/color/sc.svg'
 import logoSKY from 'cryptocurrency-icons/svg/color/sky.svg'
 import logoSMART from 'cryptocurrency-icons/svg/color/smart.svg'
 import logoSNGLS from 'cryptocurrency-icons/svg/color/sngls.svg'
 import logoSNT from 'cryptocurrency-icons/svg/color/snt.svg'
+import logoSPHTX from 'cryptocurrency-icons/svg/color/sphtx.svg'
+import logoSRN from 'cryptocurrency-icons/svg/color/srn.svg'
+import logoSTART from 'cryptocurrency-icons/svg/color/start.svg'
 import logoSTEEM from 'cryptocurrency-icons/svg/color/steem.svg'
 import logoSTORJ from 'cryptocurrency-icons/svg/color/storj.svg'
+import logoSTORM from 'cryptocurrency-icons/svg/color/storm.svg'
 import logoSTRAT from 'cryptocurrency-icons/svg/color/strat.svg'
 import logoSUB from 'cryptocurrency-icons/svg/color/sub.svg'
+import logoSYS from 'cryptocurrency-icons/svg/color/sys.svg'
 import logoTAAS from 'cryptocurrency-icons/svg/color/taas.svg'
+import logoTAU from 'cryptocurrency-icons/svg/color/tau.svg'
 import logoTKN from 'cryptocurrency-icons/svg/color/tkn.svg'
+import logoTNC from 'cryptocurrency-icons/svg/color/tnc.svg'
 import logoTRX from 'cryptocurrency-icons/svg/color/trx.svg'
+import logoTZC from 'cryptocurrency-icons/svg/color/tzc.svg'
 import logoUBQ from 'cryptocurrency-icons/svg/color/ubq.svg'
 import logoUSD from 'cryptocurrency-icons/svg/color/usd.svg'
 import logoUSDT from 'cryptocurrency-icons/svg/color/usdt.svg'
 import logoVEN from 'cryptocurrency-icons/svg/color/ven.svg'
 import logoVERI from 'cryptocurrency-icons/svg/color/veri.svg'
+import logoVIA from 'cryptocurrency-icons/svg/color/via.svg'
+import logoVIVO from 'cryptocurrency-icons/svg/color/vivo.svg'
+import logoVRC from 'cryptocurrency-icons/svg/color/vrc.svg'
 import logoVTC from 'cryptocurrency-icons/svg/color/vtc.svg'
 import logoWAVES from 'cryptocurrency-icons/svg/color/waves.svg'
+import logoWAX from 'cryptocurrency-icons/svg/color/wax.svg'
 import logoWTC from 'cryptocurrency-icons/svg/color/wtc.svg'
+import logoXBC from 'cryptocurrency-icons/svg/color/xbc.svg'
 import logoXCP from 'cryptocurrency-icons/svg/color/xcp.svg'
+import logoXDN from 'cryptocurrency-icons/svg/color/xdn.svg'
 import logoXEM from 'cryptocurrency-icons/svg/color/xem.svg'
 import logoXLM from 'cryptocurrency-icons/svg/color/xlm.svg'
+import logoXMG from 'cryptocurrency-icons/svg/color/xmg.svg'
 import logoXMR from 'cryptocurrency-icons/svg/color/xmr.svg'
+import logoXMY from 'cryptocurrency-icons/svg/color/xmy.svg'
+import logoXP from 'cryptocurrency-icons/svg/color/xp.svg'
+import logoXPM from 'cryptocurrency-icons/svg/color/xpm.svg'
+import logoXRB from 'cryptocurrency-icons/svg/color/nano.svg' // temporary during Nano rebrand
+import logoXRP from 'cryptocurrency-icons/svg/color/xrp.svg'
+import logoXTZ from 'cryptocurrency-icons/svg/color/xtz.svg'
 import logoXUC from 'cryptocurrency-icons/svg/color/xuc.svg'
+import logoXVC from 'cryptocurrency-icons/svg/color/xvc.svg'
 import logoXVG from 'cryptocurrency-icons/svg/color/xvg.svg'
 import logoXZC from 'cryptocurrency-icons/svg/color/xzc.svg'
+import logoZCL from 'cryptocurrency-icons/svg/color/zcl.svg'
 import logoZEC from 'cryptocurrency-icons/svg/color/zec.svg'
 import logoZEN from 'cryptocurrency-icons/svg/color/zen.svg'
 import logoZRX from 'cryptocurrency-icons/svg/color/zrx.svg'
-import logoXRB from 'cryptocurrency-icons/svg/color/xrb.svg'
-import logoXRP from 'cryptocurrency-icons/svg/color/xrp.svg'
-import logoXTZ from 'cryptocurrency-icons/svg/color/xtz.svg'
-
 
 const logos = {
-  'ACT': logoACT,
-  'ADA': logoADA,
-  'ADX': logoADX,
-  'AE': logoAE,
-  'AION': logoAION,
-  'AMP': logoAMP,
-  'ANT': logoANT,
-  'ARDR': logoARDR,
-  'ARK': logoARK,
-  'ATM': logoATM,
-  'BAT': logoBAT,
-  'BAY': logoBAY,
-  'BCC': logoBCC,
-  'BCH': logoBCH,
-  'BCN': logoBCN,
-  'BDL': logoBDL,
-  'BLCN': logoBLCN,
-  'BLOCK': logoBLOCK,
-  'BNB': logoBNB,
-  'BNT': logoBNT,
-  'BQ': logoBQ,
-  'BQX': logoBQX,
-  'BTC': logoBTC,
-  'BTCD': logoBTCD,
-  'BTCZ': logoBTCZ,
-  'BTG': logoBTG,
-  'BTM': logoBTM,
-  'BTS': logoBTS,
-  'BTX': logoBTX,
-  'CNX': logoCNX,
-  'CNY': logoCNY,
-  'CVC': logoCVC,
-  'DASH': logoDASH,
-  'DATA': logoDATA,
-  'DCN': logoDCN,
-  'DCR': logoDCR,
-  'DENT': logoDENT,
-  'DGB': logoDGB,
-  'DGD': logoDGD,
-  'DOGE': logoDOGE,
-  'DRGN': logoDRGN,
-  'EDG': logoEDG,
-  'EDOGE': logoEDOGE,
-  'ELF': logoELF,
-  'EMC': logoEMC,
-  'EMC2': logoEMC2,
-  'ENG': logoENG,
-  'EOS': logoEOS,
-  'ETC': logoETC,
-  'ETH': logoETH,
-  'ETHOS': logoETHOS,
-  'ETN': logoETN,
-  'ETP': logoETP,
-  'EUR': logoEUR,
-  'EXMO': logoEXMO,
-  'FAIR': logoFAIR,
-  'FCT': logoFCT,
-  'FIL': logoFIL,
-  'FUN': logoFUN,
-  'GAME': logoGAME,
-  'GAS': logoGAS,
-  'GBP': logoGBP,
-  'GBYTE': logoGBYTE,
-  'GNO': logoGNO,
-  'GNT': logoGNT,
-  'GRS': logoGRS,
-  'GUP': logoGUP,
-  'GXS': logoGXS,
-  'HSR': logoHSR,
-  'ICN': logoICN,
-  'ICX': logoICX,
-  'JPY': logoJPY,
-  'KCS': logoKCS,
-  'KMD': logoKMD,
-  'KNC': logoKNC,
-  'KRB': logoKRB,
-  'LKK': logoLKK,
-  'LRC': logoLRC,
-  'LSK': logoLSK,
-  'LTC': logoLTC,
-  'MAID': logoMAID,
-  'MANA': logoMANA,
-  'MCAP': logoMCAP,
-  'MCO': logoMCO,
-  'MIOTA': logoMIOTA,
-  'MLN': logoMLN,
-  'MNX': logoMNX,
-  'MONA': logoMONA,
-  'MTL': logoMTL,
-  'MUSIC': logoMUSIC,
-  'NAV': logoNAV,
-  'NEBL': logoNEBL,
-  'NEO': logoNEO,
-  'NLG': logoNLG,
-  'NMC': logoNMC,
-  'NXS': logoNXS,
-  'NXT': logoNXT,
-  'OMG': logoOMG,
-  'PART': logoPART,
-  'PAY': logoPAY,
-  'PIVX': logoPIVX,
-  'POT': logoPOT,
-  'POWR': logoPOWR,
-  'PPC': logoPPC,
-  'PPT': logoPPT,
-  'PURA': logoPURA,
-  'QASH': logoQASH,
-  'QIWI': logoQIWI,
-  'QTUM': logoQTUM,
-  'RCN': logoRCN,
-  'RDD': logoRDD,
-  'RDN': logoRDN,
-  'REP': logoREP,
-  'REQ': logoREQ,
-  'RHOC': logoRHOC,
-  'RLC': logoRLC,
-  'RUB': logoRUB,
-  'SALT': logoSALT,
-  'SAN': logoSAN,
-  'SBERBANK': logoSBERBANK,
-  'SC': logoSC,
-  'SKY': logoSKY,
-  'SMART': logoSMART,
-  'SNGLS': logoSNGLS,
-  'SNT': logoSNT,
-  'STEEM': logoSTEEM,
-  'STORJ': logoSTORJ,
-  'STRAT': logoSTRAT,
-  'SUB': logoSUB,
-  'TAAS': logoTAAS,
-  'TKN': logoTKN,
-  'TRX': logoTRX,
-  'UBQ': logoUBQ,
-  'USD': logoUSD,
-  'USDT': logoUSDT,
-  'VEN': logoVEN,
-  'VERI': logoVERI,
-  'VTC': logoVTC,
-  'WAVES': logoWAVES,
-  'WTC': logoWTC,
-  'XCP': logoXCP,
-  'XEM': logoXEM,
-  'XLM': logoXLM,
-  'XMR': logoXMR,
-  'XUC': logoXUC,
-  'XVG': logoXVG,
-  'XZC': logoXZC,
-  'ZEC': logoZEC,
-  'ZEN': logoZEN,
-  'ZRX': logoZRX,
-  'XRB': logoXRB,
-  'XRP': logoXRP,
-  'XTZ': logoXTZ,
+  ACT: logoACT,
+  ADA: logoADA,
+  ADX: logoADX,
+  AE: logoAE,
+  AION: logoAION,
+  AMP: logoAMP,
+  ANT: logoANT,
+  ARDR: logoARDR,
+  ARK: logoARK,
+  ATM: logoATM,
+  BAT: logoBAT,
+  BAY: logoBAY,
+  BCC: logoBCC,
+  BCH: logoBCH,
+  BCN: logoBCN,
+  BDL: logoBDL,
+  BELA: logoBELA,
+  BLCN: logoBLCN,
+  BLK: logoBLK,
+  BLOCK: logoBLOCK,
+  BNB: logoBNB,
+  BNT: logoBNT,
+  BQ: logoBQ,
+  BQX: logoBQX,
+  BTC: logoBTC,
+  BTCD: logoBTCD,
+  BTCZ: logoBTCZ,
+  BTG: logoBTG,
+  BTM: logoBTM,
+  BTS: logoBTS,
+  BTX: logoBTX,
+  BURST: logoBURST,
+  CDN: logoCDN,
+  CLAM: logoCLAM,
+  CLOAK: logoCLOAK,
+  CND: logoCND,
+  CNX: logoCNX,
+  CNY: logoCNY,
+  CRED: logoCRED,
+  CRPT: logoCRPT,
+  CVC: logoCVC,
+  DASH: logoDASH,
+  DAT: logoDAT,
+  DATA: logoDATA,
+  DBC: logoDBC,
+  DCN: logoDCN,
+  DCR: logoDCR,
+  DENT: logoDENT,
+  DGB: logoDGB,
+  DGD: logoDGD,
+  DOGE: logoDOGE,
+  DRGN: logoDRGN,
+  EDG: logoEDG,
+  EDOGE: logoEDOGE,
+  ELF: logoELF,
+  ELLA: logoELLA,
+  EMC: logoEMC,
+  EMC2: logoEMC2,
+  ENG: logoENG,
+  EOS: logoEOS,
+  ETC: logoETC,
+  ETH: logoETH,
+  ETHOS: logoETHOS,
+  ETN: logoETN,
+  ETP: logoETP,
+  EUR: logoEUR,
+  EVX: logoEVX,
+  EXMO: logoEXMO,
+  EXP: logoEXP,
+  FAIR: logoFAIR,
+  FCT: logoFCT,
+  FIL: logoFIL,
+  FLDC: logoFLDC,
+  FLO: logoFLO,
+  FUN: logoFUN,
+  GAME: logoGAME,
+  GAS: logoGAS,
+  GBP: logoGBP,
+  GBX: logoGBX,
+  GBYTE: logoGBYTE,
+  GNO: logoGNO,
+  GNT: logoGNT,
+  GRC: logoGRC,
+  GRS: logoGRS,
+  GUP: logoGUP,
+  GXS: logoGXS,
+  HSR: logoHSR,
+  HUC: logoHUC,
+  HUSH: logoHUSH,
+  ICN: logoICN,
+  ICX: logoICX,
+  INS: logoINS,
+  IOST: logoIOST,
+  JPY: logoJPY,
+  KCS: logoKCS,
+  KIN: logoKIN,
+  KMD: logoKMD,
+  KNC: logoKNC,
+  KRB: logoKRB,
+  LBC: logoLBC,
+  LINK: logoLINK,
+  LKK: logoLKK,
+  LRC: logoLRC,
+  LSK: logoLSK,
+  LTC: logoLTC,
+  MAID: logoMAID,
+  MANA: logoMANA,
+  MCAP: logoMCAP,
+  MCO: logoMCO,
+  MED: logoMED,
+  MIOTA: logoMIOTA,
+  MKR: logoMKR,
+  MLN: logoMLN,
+  MNX: logoMNX,
+  MONA: logoMONA,
+  MTL: logoMTL,
+  MUSIC: logoMUSIC,
+  NANO: logoNANO,
+  NAS: logoNAS,
+  NAV: logoNAV,
+  NDZ: logoNDZ,
+  NEBL: logoNEBL,
+  NEO: logoNEO,
+  NEOS: logoNEOS,
+  NGC: logoNGC,
+  NLC2: logoNLC2,
+  NLG: logoNLG,
+  NMC: logoNMC,
+  NXS: logoNXS,
+  NXT: logoNXT,
+  OMG: logoOMG,
+  OMNI: logoOMNI,
+  OST: logoOST,
+  OX: logoOX,
+  PAC: logoPAC,
+  PART: logoPART,
+  PASL: logoPASL,
+  PAY: logoPAY,
+  PINK: logoPINK,
+  PIRL: logoPIRL,
+  PIVX: logoPIVX,
+  PLR: logoPLR,
+  POE: logoPOE,
+  POT: logoPOT,
+  POWR: logoPOWR,
+  PPC: logoPPC,
+  PPT: logoPPT,
+  PURA: logoPURA,
+  QASH: logoQASH,
+  QIWI: logoQIWI,
+  QLC: logoQLC,
+  QSP: logoQSP,
+  QTUM: logoQTUM,
+  RADS: logoRADS,
+  RCN: logoRCN,
+  RDD: logoRDD,
+  RDN: logoRDN,
+  REP: logoREP,
+  REQ: logoREQ,
+  RHOC: logoRHOC,
+  RIC: logoRIC,
+  RISE: logoRISE,
+  RLC: logoRLC,
+  RPX: logoRPX,
+  RUB: logoRUB,
+  SALT: logoSALT,
+  SAN: logoSAN,
+  SBD: logoSBD,
+  SBERBANK: logoSBERBANK,
+  SC: logoSC,
+  SKY: logoSKY,
+  SMART: logoSMART,
+  SNGLS: logoSNGLS,
+  SNT: logoSNT,
+  SPHTX: logoSPHTX,
+  SRN: logoSRN,
+  START: logoSTART,
+  STEEM: logoSTEEM,
+  STORJ: logoSTORJ,
+  STORM: logoSTORM,
+  STRAT: logoSTRAT,
+  SUB: logoSUB,
+  SYS: logoSYS,
+  TAAS: logoTAAS,
+  TAU: logoTAU,
+  TKN: logoTKN,
+  TNC: logoTNC,
+  TRX: logoTRX,
+  TZC: logoTZC,
+  UBQ: logoUBQ,
+  USD: logoUSD,
+  USDT: logoUSDT,
+  VEN: logoVEN,
+  VERI: logoVERI,
+  VIA: logoVIA,
+  VIVO: logoVIVO,
+  VRC: logoVRC,
+  VTC: logoVTC,
+  WAVES: logoWAVES,
+  WAX: logoWAX,
+  WTC: logoWTC,
+  XBC: logoXBC,
+  XCP: logoXCP,
+  XDN: logoXDN,
+  XEM: logoXEM,
+  XLM: logoXLM,
+  XMG: logoXMG,
+  XMR: logoXMR,
+  XMY: logoXMY,
+  XP: logoXP,
+  XPM: logoXPM,
+  XRB: logoXRB,
+  XRP: logoXRP,
+  XTZ: logoXTZ,
+  XUC: logoXUC,
+  XVC: logoXVC,
+  XVG: logoXVG,
+  XZC: logoXZC,
+  ZCL: logoZCL,
+  ZEC: logoZEC,
+  ZEN: logoZEN,
+  ZRX: logoZRX,
 };
 
 export default logos;
